c96c999a018085605b58071deaae097f3d61f123
Martin Lee <144554274+MartinLee04@users.noreply.github.com>
2025-05-08 12:56:37 -0400

    Update rmpApi.js

--- rate-my-professor-extension/src/api/rmpApi.js
+++ rate-my-professor-extension/src/api/rmpApi.js
@@ -2,7 +2,6 @@ import { fetchProfessorData } from './rmpScraper.js';
 
 export const rmpApi = {
     async searchSchool(schoolName) {
-        // Optionally implement this or hardcode the ID
         return [{ node: { id: 'U2Nob29sLTEwNzM=', name: 'University of Waterloo' } }];
     },
     async getProfessorRatingAtSchoolId(professorName, schoolId) {
